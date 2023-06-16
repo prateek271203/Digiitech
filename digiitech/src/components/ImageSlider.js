8ace252add8e883fc98d1873aeeb835f92aa3c06
Saif Ali Khan <saif7862254j@gmail.com>
2023-06-16 16:03:43 +0530

    updated

--- digiitech/src/components/ImageSlider.js
+++ digiitech/src/components/ImageSlider.js
@@ -1,45 +1,59 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+import React, { useEffect, useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import Image1 from "./.../../image1.jpg";
-// Import Swiper styles
+import SwiperCore, { Autoplay, Navigation } from "swiper";
+
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import "./ImageSlider.css";
 
-// import required modules
-import { Autoplay, Pagination, Navigation } from "swiper";
+SwiperCore.use([Autoplay, Navigation]);
 
 function ImageSlider() {
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    const swiperInstance = swiperRef.current.swiper;
+
+    const handleKeyDown = (event) => {
+      if (event.key === "ArrowLeft") {
+        swiperInstance.slidePrev();
+      } else if (event.key === "ArrowRight") {
+        swiperInstance.slideNext();
+      }
+
+      // Restart autoplay timer
+      swiperInstance.autoplay.start();
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
     <>
       <Swiper
+        ref={swiperRef}
+        slidesPerView={1}
         spaceBetween={30}
         centeredSlides={true}
+        loop={true}
         autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
+          delay: 4000,
+          disableOnInteraction: false, // Allow autoplay to continue even when interacting with slides
         }}
         navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <img src={Image1}></img>
-        </SwiperSlide>
+        <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </>
   );
