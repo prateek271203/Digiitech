ce010653643dd32adcd2c6b36f2bb0e88733ffe4
Saif Ali Khan <saif7862254j@gmail.com>
2023-06-17 22:34:08 +0530

    updated

--- digiitech/src/components/Cards.js
+++ digiitech/src/components/Cards.js
@@ -1,39 +1,39 @@
 import React from "react";
 import "./Cards.css";
-const Cards = () => {
+function Cards() {
   return (
-    <div class="container">
-      <div class="box">
-        <div class="title">
+    <div className="container">
+      <div className="box">
+        <div className="title">
           Become a <br />
           <h5>Web Developer</h5>
         </div>
-        <div class="content">
+        <div className="content">
           <span>Know More</span>
         </div>
       </div>
 
-      <div class="box">
-        <div class="title">
+      <div className="box">
+        <div className="title">
           Become a <br />
           <h5>Data Scientist</h5>
         </div>
-        <div class="content">
+        <div className="content">
           <span>Know more</span>
         </div>
       </div>
 
-      <div class="box">
-        <div class="title">
+      <div className="box">
+        <div className="title">
           Become a <br />
           <h5>DevOps Engineer</h5>
         </div>
-        <div class="content">
+        <div className="content">
           <span>Know More</span>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Cards;
