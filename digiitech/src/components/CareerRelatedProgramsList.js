09e5e38fd6d8d4bba0ca0086fd54138254e687b4
Saif Ali Khan <saif7862254j@gmail.com>
2023-06-17 13:53:13 +0530

    updated

--- digiitech/src/components/CareerRelatedProgramsList.js
+++ digiitech/src/components/CareerRelatedProgramsList.js
@@ -1,40 +1,42 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 import Tabs from "./Tabs";
 //import courses from '../utils/data';
 import Course from "./Course";
 //import CareerRelatedPrograms from "./CareerRelatedPrograms";
-import { useCoursesContext } from '../context/courses_context';
+import { useCoursesContext } from "../context/courses_context";
 
 const CareerRelatedProgramsList = () => {
-  const {courses} = useCoursesContext();
+  const { courses } = useCoursesContext();
 
   return (
     <CoursesListWrapper>
-      <div className='container'>
-        <div className='courses-list-top'>
+      <div className="container">
+        <div className="courses-list-top">
           <h2>Career Related Programs</h2>
         </div>
         <div class="tabs-body">
-        {
-            courses.filter(CareerRelatedPrograms => CareerRelatedPrograms.category).map((CareerRelatedPrograms) => (
-              <Course key = {CareerRelatedPrograms.id} {...CareerRelatedPrograms} />
-            ))
-        }
+          {courses
+            .filter((CareerRelatedPrograms) => CareerRelatedPrograms.category)
+            .map((CareerRelatedPrograms) => (
+              <Course
+                key={CareerRelatedPrograms.id}
+                {...CareerRelatedPrograms}
+              />
+            ))}
         </div>
 
-
-     <Tabs courses = {courses} />
+        <Tabs courses={courses} />
       </div>
     </CoursesListWrapper>
-  )
-}
+  );
+};
 
 const CoursesListWrapper = styled.div`
   padding: 40px 0;
-  .courses-list-top p{
+  .courses-list-top p {
     font-size: 1.8rem;
   }
 `;
 
-export default CareerRelatedProgramsList
+export default CareerRelatedProgramsList;
